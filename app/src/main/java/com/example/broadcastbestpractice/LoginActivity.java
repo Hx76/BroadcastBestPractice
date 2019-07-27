a97bb0c87ff86ec429e0e62b984658ef74e919ff
Hx76 <43670079+Hx76@users.noreply.github.com>
2019-07-27 16:51:42 +0800

    强制下线练习

---
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.broadcastbestpractice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class LoginActivity extends BaseActivity {
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = username.getText().toString();
+                String pwd = password.getText().toString();
+                if (uname.equals("admin")&&pwd.equals("123456")) {
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(LoginActivity.this,"用户名或密码输入不正确",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
